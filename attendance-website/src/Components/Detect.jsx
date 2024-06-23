8d748db2b77c08fb7d1963c8ebfa2e377cdd9ab3
lenglee <lengleevaja@gmail.com>
2024-06-23 10:30:33 +0700

    code lenglee

--- attendance-website/src/Components/Detect.jsx
+++ attendance-website/src/Components/Detect.jsx
@@ -1,6 +1,11 @@
 import { FaHouseUser } from "react-icons/fa";
 import { IoCameraOutline } from "react-icons/io5";
 import { FaFileWord } from "react-icons/fa";
+import { BsPersonBoundingBox } from "react-icons/bs";
+import { SiGoogleclassroom } from "react-icons/si";
+import { BsCalendar2Date } from "react-icons/bs";
+import { CiTimer } from "react-icons/ci";
+import { GiTimeTrap } from "react-icons/gi";
 const Detect = () => {
   return (
    <div className="detect-section">
@@ -10,28 +15,28 @@ const Detect = () => {
                <IoCameraOutline size={30} className="icon"/>
             </div>
             <div className="title">
-            ເປີດກ້ອງເພື່ອບັນທຶກເຂົ້າຫ້ອງ
+            ເປີດກ້ອງຂອງທ່ານພື່ອບັນທຶກລາຍຊື່
             </div>
             <div className="button-container">
-               <button className="btn">Open Camera</button>
+               <button className="btn">ເປີດກ້ອງ</button>
             </div>
         </div>
         <div className='detect-right'>
-            <h3>ເປີດກ້ອງເພື່ອບັນທຶກເຂົ້າຫ້ອງ</h3>
+            <h3>ລາຍລະອຽດຂອງການບັນທືກລາຍຊື່</h3>
             <div className="detect-content">
-                <span><FaFileWord  size={25}  className="icon"  style={{color: "var(--main-color)"}}/></span><div>Lorem ipsum, dolor sit amet consectetur ?</div>
+                <span><BsPersonBoundingBox  size={25}  className="icon"  style={{color: "var(--main-color)"}}/></span><div>ສະບາຍດີ <strong>ທ້າວ ເລັ່ງລີ ວາຈາ</strong></div>
             </div>
             <div className="detect-content">
-                <span><FaFileWord  size={25}  className="icon"  style={{color: "var(--main-color)"}}/></span><div>Lorem ipsum, dolor sit amet consectetur ?</div>
+                <span><SiGoogleclassroom  size={25}  className="icon"  style={{color: "var(--main-color)"}}/></span><div>ອາຈານ ຈັນທະສິດ ຫ້ອງ:&ensp;4CS1</div>
             </div>
             <div className="detect-content">
-                <span><FaFileWord  size={25}  className="icon"  style={{color: "var(--main-color)"}}/></span><div>Lorem ipsum, dolor sit amet consectetur ?</div>
+                <span><BsCalendar2Date  size={25}  className="icon"  style={{color: "var(--main-color)"}}/></span><div>ວັນຈັນ,&ensp; ວັນທີ 13 ຕຸລາ 2022</div>
             </div>
             <div className="detect-content">
-                <span><FaFileWord  size={25}  className="icon"  style={{color: "var(--main-color)"}}/></span><div>Lorem ipsum, dolor sit amet consectetur ?</div>
+                <span><CiTimer  size={25}  className="icon"  style={{color: "var(--main-color)"}}/></span><div>ເວລາ:&ensp; 12:00-14:00,&ensp; ລະຫັດຫ້ອງ: &ensp;FNS125432</div>
             </div>
             <div className="detect-content">
-                <span><FaFileWord  size={25}  className="icon"  style={{color: "var(--main-color)"}}/></span><div>Lorem ipsum, dolor sit amet consectetur ?</div>
+                <span><GiTimeTrap  size={25}  className="icon"  style={{color: "var(--main-color)"}}/></span><div>ຊົ່ວໂມງທີ່:&ensp; 2</div>
             </div>
         </div>
     </div>
